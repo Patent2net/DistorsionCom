a2cbba9d8fc37c8ef4a4cc7c72dc586aecfc2b7a
Patent2net <patent2net@gmail.com>
2021-12-01 15:08:06 +0100

    Update outils.py
    amélioration de l'analyse lexicale html

--- outils.py
+++ outils.py
@@ -7,6 +7,22 @@ import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib import cm, colors
 from urllib import parse as parse
+from bs4 import BeautifulSoup
+from bs4.element import Comment
+import urllib.request
+
+def tag_visible(element):
+    if element.parent.name in ['style', 'script', 'head', 'title', 'meta', '[document]']:
+        return False
+    if isinstance(element, Comment):
+        return False
+    return True
+
+def text_from_html(body):
+    soup = BeautifulSoup(body, 'html.parser')
+    texts = soup.findAll(text=True)
+    visible_texts = filter(tag_visible, texts)
+    return u" ".join(t.strip() for t in visible_texts)
 
 class SimpleGroupedColorFunc(object):
     """Create a color function object which assigns EXACT colors
